47ad8f2a5dcb9f33593d16c9533e8f2f647bb632
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-11 02:53:17 +0700

    a

---
+++ exercise/company-profile/src/app/layout.tsx
@@ -0,0 +1,26 @@
+
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Navbar } from '@/components/Navbar'
+import { Footer } from '@/components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+
+        <Navbar />
+        
+        {children}
+
+        <Footer />
+      </body>
+    </html>
+  )
+}
