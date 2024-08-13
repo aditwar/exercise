4289c939365778abd7ff90ee55e2b75343d824b8
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-13 22:04:13 +0700

    a

--- exercise/company-profile/src/lib/blog.ts
+++ exercise/company-profile/src/lib/blog.ts
@@ -1,10 +1,10 @@
-const base_url = process.env.BASE_URL
-const space_id = process.env.SPACE_ID
-const token = process.env.TOKEN
+const base_url = process.env.BASE_URL_CONTENTFUL
+const space_id = process.env.SPACE_ID_CONTENTFUL
+const token = process.env.TOKEN_CONTENFUL
 import resolveResponse from 'contentful-resolve-response'
 
 export const getBlogs = async () => {
-    const res = await fetch(`${base_url}/spaces/${space_id}/environments/master/entries?access_token=${token}&content_type=blog&include=10`, { next: {  revalidate: 10 } })
+    const res = await fetch(`https://cdn.contentful.com/spaces/qdgn5flew5hp/environments/master/entries?access_token=Zvg_68T3Y01hLvAuo4QGvMft7AVM-1DZ8IHhGXrAz80&content_type=blog&include=10`, { next: {  revalidate: 10 } })
     const data = await res.json()
 
     const response = {
@@ -18,7 +18,7 @@ export const getBlogs = async () => {
 }
 
 export const getBlogSlug = async (slug: string) => {
-    const res = await fetch(`${base_url}/spaces/${space_id}/environments/master/entries?access_token=${token}&content_type=blog&include=10&fields.slug=${slug}`, { next: {  revalidate: 3600 } })
+    const res = await fetch(`https://cdn.contentful.com/spaces/qdgn5flew5hp/environments/master/entries?access_token=Zvg_68T3Y01hLvAuo4QGvMft7AVM-1DZ8IHhGXrAz80&content_type=blog&include=10&fields.slug`, { next: {  revalidate: 3600 } })
     const data = await res.json()
 
     const response = {
