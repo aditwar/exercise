bb447fbd9ee1e5703e58efd75dee60081961450f
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-20 20:13:35 +0700

    a

---
+++ 08.08_Node.js/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+
+const PORT: number = 8000
+
+const app: Application = express()
+
+app.get('/api', (req: Request, res: Response) => {
+    res.status(200).send({
+        status: 'ok',
+        msg: 'Welcome to my API'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('[API] : http://localhost:${PORT}/api');
+    
+})
