bc86f88accbc302a3e1d6801d44e8e7e50fcc557
Aditya Wardana <wardanaaditya49@gmail.com>
2024-09-10 19:24:00 +0700

    a

--- 08.13_api_expense/sql_database.sql
+++ 08.13_api_expense/sql_database.sql
@@ -12,3 +12,12 @@ CREATE TABLE expense (
 SHOW TABLES;
 ALTER TABLE expense
 ADD date DATE NOT NULL;
+
+INSERT INTO expense(title, type, category, nominal, date) VALUES('Beli Nasi Padang', 'expense', 'food', 20000, '2024-08-01');
+
+INSERT INTO expense(title, type, category, nominal, date)
+VALUES
+	('Beli Bensin', 'expense', 'transport', 20000, '2024-08-05'),
+    ('Gaji Bulan Juli', 'income', 'sallary', 200000, '2024-08-10');
+
+SELECT * FROM expense
